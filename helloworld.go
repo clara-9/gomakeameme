ebcdae28c93406f96965118906f6fd406aaa782a
clara <clara.rull@estudiantat.upc.edu>
2021-02-15 00:18:07 +0100

    hello world get api response

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+   "io/ioutil"
+   "log"
+   "net/http"
+)
 
 func main() {
-    fmt.Println("hello world")
+   resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+   if err != nil {
+      log.Fatalln(err)
+   }
+//We Read the response body on the line below.
+   body, err := ioutil.ReadAll(resp.Body)
+   if err != nil {
+      log.Fatalln(err)
+   }
+//Convert the body to type string
+   sb := string(body)
+   log.Printf(sb)
 }
