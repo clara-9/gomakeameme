9ee2277859a970d379004e8293dcb95a0c9f7e4d
clara <clara.rull@estudiantat.upc.edu>
2021-02-15 17:55:53 +0100

    separate into functions

--- helloworld.go
+++ helloworld.go
@@ -9,6 +9,30 @@ import (
 	"golang.org/x/oauth2/clientcredentials"
 )
 
+func getStatus(statusNum int64, client *twitter.Client){
+   // status show
+	statusShowParams := &twitter.StatusShowParams{}
+	tweet, _, _ := client.Statuses.Show(statusNum, statusShowParams)
+   getTextFromTweet(tweet)
+}
+
+func getRichStatus(statusNum int64, client *twitter.Client){
+   // HTML status show
+	statusOembedParams := &twitter.StatusOEmbedParams{ID: statusNum, MaxWidth: 500}
+	oembed, _, _ := client.Statuses.OEmbed(statusOembedParams)
+	fmt.Printf("OEMBED TWEET:\n%+v\n", oembed)
+}
+
+func getUserTimeline (screenName string, client *twitter.Client){
+   // user timeline
+	userTimelineParams := &twitter.UserTimelineParams{ScreenName: screenName, Count: 2}
+	tweets, _, _ := client.Timelines.UserTimeline(userTimelineParams)
+	fmt.Printf("USER TIMELINE:\n%+v\n", tweets)
+}
+func getTextFromTweet(tweet *twitter.Tweet){
+   fmt.Println(tweet.Text)
+}
+
 func main() {
 	
    consumerKey:= os.Getenv("TWITTER_CONSUMER_KEY")
@@ -26,43 +50,12 @@ func main() {
 	// Twitter client
 	client := twitter.NewClient(httpClient)
 
-	// user show
-	userShowParams := &twitter.UserShowParams{ScreenName: "golang"}
-	user, _, _ := client.Users.Show(userShowParams)
-	fmt.Printf("USERS SHOW:\n%+v\n", user)
-
-	// users lookup
-	userLookupParams := &twitter.UserLookupParams{ScreenName: []string{"golang", "gophercon"}}
-	users, _, _ := client.Users.Lookup(userLookupParams)
-	fmt.Printf("USERS LOOKUP:\n%+v\n", users)
-
 	// status show
-	statusShowParams := &twitter.StatusShowParams{}
-	tweet, _, _ := client.Statuses.Show(584077528026849280, statusShowParams)
-	fmt.Printf("STATUSES SHOW:\n%+v\n", tweet)
-
-	// statuses lookup
-	statusLookupParams := &twitter.StatusLookupParams{ID: []int64{20}, TweetMode: "extended"}
-	tweets, _, _ := client.Statuses.Lookup([]int64{573893817000140800}, statusLookupParams)
-	fmt.Printf("STATUSES LOOKUP:\n%+v\n", tweets)
+	getStatus(1353403903690354688, client)
 
 	// oEmbed status
-	statusOembedParams := &twitter.StatusOEmbedParams{ID: 691076766878691329, MaxWidth: 500}
-	oembed, _, _ := client.Statuses.OEmbed(statusOembedParams)
-	fmt.Printf("OEMBED TWEET:\n%+v\n", oembed)
+	getRichStatus(1353403903690354688, client)
 
-	// user timeline
-	userTimelineParams := &twitter.UserTimelineParams{ScreenName: "golang", Count: 2}
-	tweets, _, _ = client.Timelines.UserTimeline(userTimelineParams)
-	fmt.Printf("USER TIMELINE:\n%+v\n", tweets)
-
-	// search tweets
-	searchTweetParams := &twitter.SearchTweetParams{
-		Query:     "happy birthday",
-		TweetMode: "extended",
-		Count:     3,
-	}
-	search, _, _ := client.Search.Tweets(searchTweetParams)
-	fmt.Printf("SEARCH TWEETS:\n%+v\n", search)
-	fmt.Printf("SEARCH METADATA:\n%+v\n", search.Metadata)
+   // user timeline
+   getUserTimeline("hacktheurban", client)
 }
