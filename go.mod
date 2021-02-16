82549b3334c23064cf6671ddcf9978c39ab9a080
clara <clara.rull@estudiantat.upc.edu>
2021-02-16 22:39:23 +0100

    finished app

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gomakeameme
+
+go 1.15
+
+require (
+	github.com/dghubble/go-twitter v0.0.0-20201011215211-4b180d0cc78d
+	github.com/sfreiberg/gotwilio v0.0.0-20201211181435-c426a3710ab5
+	golang.org/x/oauth2 v0.0.0-20210216194517-16ff1888fd2e
+)
--- gomakeameme.go
+++ gomakeameme.go
@@ -1,39 +1,78 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
+	"encoding/json"
+	"io/ioutil"
 
 	"github.com/sfreiberg/gotwilio"
 )
 
 type message struct {
-	Content string
-	To      string
-	Medium  string
+	Content  string
+	MediaURL []string
+	To       string
+	Medium   string
+}
+
+type meme struct {
+	Success bool
+	Data    map[string]interface{}
 }
 
 //SendWhatsapp - Sends Whatsapp text message
 func SendWhatsapp(m message, credentials *gotwilio.Twilio) (*gotwilio.SmsResponse, error) {
-	resp, _, err := credentials.SendWhatsApp("+14155238886", m.To, m.Content, "", "")
+	resp, _, err := credentials.SendWhatsAppMedia("+14155238886", m.To, m.Content, m.MediaURL, "", "")
 	if err != nil {
 		return nil, err
 	}
 	return resp, nil
 }
 
-//UseTextWhatsapp Sets message text and forwards
-func UseTextWhatsapp(text string) {
-	newSms := message{
-		To:      "+phnoe",
-		Content: text,
-		Medium:  "whatsapp",
+func getMeme(text0, text1 string) string {
+	postBody, _ := json.Marshal(map[string]string{})
+	responseBody := bytes.NewBuffer(postBody)
+
+	url := fmt.Sprintf("https://api.imgflip.com/caption_image?template_id=102156234&text0=%s&text1=%s&username=clarara&password=%s",
+		text0, text1, os.Getenv("IMGFLIP_PASSWORD"))
+	resp, err := http.Post(url, "application/json", responseBody)
+	//Handle Error
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
 	}
-	twilio := gotwilio.NewTwilioClient(os.Getenv("TwilioAccountSid"), os.Getenv("TwilioAuthToken"))
-	SendWhatsapp(newSms, twilio)
-	fmt.Println("Sent!")
+	defer resp.Body.Close()
+	//Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sb := string(body)
+	log.Printf(sb)
+
+	var m meme
+	err = json.Unmarshal([]byte(sb), &m)
+
+	fmt.Println(m.Data["url"].(string))
+
+	memeUrl := m.Data["url"].(string)
+
+	return memeUrl
+}
+
+func parseText(text string) (string, string) {
+	text = strings.TrimSpace(text)
+	half := len(text) / 2
+	last := strings.LastIndex(text[:half], " ")
+	if last == -1 {
+		return "", text
+	}
+	return text[:last], text[last:]
 }
 
 func ExampleHandler(w http.ResponseWriter, r *http.Request) {
@@ -52,12 +91,24 @@ func ExampleHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.Form)
 
 	// Print the data back. We can use Form.Get() or Form["name"][0]
-	fmt.Println(r.Form.Get("name"))
 
-	text := r.Form.Get("message")
+	text := r.Form.Get("Body")
 
-	UseTextWhatsapp(text)
+	to := r.Form.Get("From")[len(r.Form.Get("From"))-11:]
+	fmt.Println(to)
 
+	text0, text1 := parseText(text)
+
+	memeURL := []string{getMeme(text0, text1)}
+
+	newSms := message{
+		To:       to,
+		Content:  "",
+		MediaURL: memeURL,
+		Medium:   "whatsapp",
+	}
+	twilio := gotwilio.NewTwilioClient(os.Getenv("TwilioAccountSid"), os.Getenv("TwilioAuthToken"))
+	SendWhatsapp(newSms, twilio)
 }
 
 func main() {
@@ -65,12 +116,4 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-
-	newSms := message{
-		To:      "+34695848183",
-		Content: "This is still sandbox",
-		Medium:  "whatsapp",
-	}
-	twilio := gotwilio.NewTwilioClient(os.Getenv("TwilioAccountSid"), os.Getenv("TwilioAuthToken"))
-	SendWhatsapp(newSms, twilio)
 }
